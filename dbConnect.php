ca866e306529ee0df5c9418b30d77e35783c27d7
gs02123 <gs02123@naver.com>
2017-05-28 00:48:05 +0900

    Add files via upload

---
+++ dbConnect.php
@@ -0,0 +1,12 @@
+<?php
+  $host = 'localhost';
+  $user = 'root';
+  $passWord = 'ksy1127';
+  $dbName = 'myProject';
+  $adminid = 'adminaccess';
+  $adminpw = 'adminaccess';
+ 
+  $dbConnect = new mysqli($host,$user,$passWord,$dbName);
+
+  
+?>
